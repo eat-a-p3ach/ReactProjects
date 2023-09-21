a29086017814388a36f50d565b4170f5132be8ce
Lyncy Hodge <lyncyyhodge@gmail.com>
2023-09-20 21:28:51 -0500

    React Beginner

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+// { items: [], heading: string }
+//type annotation to specify the types of various properties
+interface Props {
+  items: string[];
+  heading: string;
+  // (item: string) => void
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  // Hook: a function that allows us to tap into built in features in React - State hook tell React this component has data/state that can change over time
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
